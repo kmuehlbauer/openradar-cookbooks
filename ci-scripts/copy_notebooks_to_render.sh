fe7b26574ea9406a3a0ec09ee5a9cf9dba56aebc
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2017-09-24 12:53:02 +0200

    WIP: several fixes

---
+++ ci-scripts/copy_notebooks_to_render.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Copyright (c) 2017, openradar developers.
+# Distributed under the BSD 3-Clause license. See LICENSE for more info.
+
+mkdir $OPENRADAR_NOTEBOOKS && cp -R notebooks $OPENRADAR_NOTEBOOKS/
