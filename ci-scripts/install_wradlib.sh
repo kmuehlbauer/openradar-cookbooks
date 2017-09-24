df3dd04548ae1a61492b919ebd71e9fc84be2379
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2017-09-24 10:26:09 +0200

    WIP: fix install_wradlib.sh (+x)

--- ci-scripts/install_wradlib.sh
+++ ci-scripts/install_wradlib.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2017, openradar developers.
 # Distributed under the BSD 3-Clause license. See LICENSE for more info.
 
-# clone and install wradlib
+# shallow clone and install wradlib
 git clone --depth 1 https://github.com/wradlib/wradlib.git
 cd wradlib
 python setup.py install
